524c8f8c5f5770f6efffad7e608f1290b192f570
vaishnavireddy122 <153925403+vaishnavireddy122@users.noreply.github.com>
2024-08-22 18:30:57 +0530

    Create drop.js

---
+++ drop.js
@@ -0,0 +1,38 @@
+let dropdownBtnText = document.getElementById("drop-text");
+let span = document.getElementById("span");
+let icon = document.getElementById("icon");
+let list = document.getElementById("list");
+let input = document.getElementById("search-input");
+let listItems = document.querySelectorAll(".dropdown-list-item");
+
+dropdownBtnText.onclick = function () {
+  
+  if(list.classList.contains("show")){
+    icon.style.rotate="0deg";
+  }else{
+    icon.style.rotate="-180deg";
+  }
+  list.classList.toggle("show");
+};
+
+window.onclick = function (e) {
+  if (
+    e.target.id !== "drop-text" &&
+    e.target.id !== "icon" &&
+    e.target.id !== "span"
+  ) {
+    list.classList.remove("show");
+    icon.style.rotate = "0deg";
+  }
+};
+
+for (item of listItems) {
+  item.onclick = function (e) {
+    span.innerText = e.target.innerText;
+    if (e.target.innerText == "Everything") {
+      input.placeholder = "Search Anything...";
+    } else {
+      input.placeholder = "Search in " + e.target.innerText + "...";
+    }
+  };
+}
